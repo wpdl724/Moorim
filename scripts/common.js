a87a501a4bf7682805d1b280844414f913df9d0f
jay <wpdl724@gmail.com>
2025-07-25 17:57:22 +0900

    헤더 진행중+js

--- scripts/common.js
+++ scripts/common.js
@@ -1,1 +1,26 @@
 /* 무림 메인페이지  - 공통적인 동적 기능 JS */
+
+
+
+
+
+
+/* right_wrap (언어변경) */
+// 초기설정 : 한국어 , 영어 옵션 숨기기
+//lang_select 클릭시, .lang_open 보이기
+// lang_open이 보이는 상태라면 #lang_select 클릭시 .lang_open 숨기기
+const langOpen = document.querySelector('.lang_open');
+const langSelect = document.querySelector('#lang_select');
+console.log(langOpen,langSelect);
+langOpen.style.display = 'none'; /* 초기값 설정 - 숨기기 */
+
+let openif = false; // 숨기기
+langSelect.addEventListener('click', ()=>{
+    if(openif==false){
+        langOpen.style.display = 'flex';
+        openif = true //상태값 변경
+    }else if(openif == true){
+        langOpen.style.display = 'none';
+        openif = false;
+    }
+})
