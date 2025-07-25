9a2bbf4450eb5419decb8e1ec39c311baa9a4ab4
jay <wpdl724@gmail.com>
2025-07-25 19:01:36 +0900

    헤더 위치 수정

--- scripts/common.js
+++ scripts/common.js
@@ -9,18 +9,18 @@
 // 초기설정 : 한국어 , 영어 옵션 숨기기
 //lang_select 클릭시, .lang_open 보이기
 // lang_open이 보이는 상태라면 #lang_select 클릭시 .lang_open 숨기기
-const langOpen = document.querySelector('.lang_open');
-const langSelect = document.querySelector('#lang_select');
-console.log(langOpen,langSelect);
-langOpen.style.display = 'none'; /* 초기값 설정 - 숨기기 */
+// const langOpen = document.querySelector('.lang_open');
+// const langSelect = document.querySelector('#lang_select');
+// console.log(langOpen,langSelect);
+// langOpen.style.display = 'none'; /* 초기값 설정 - 숨기기 */
 
-let openif = false; // 숨기기
-langSelect.addEventListener('click', ()=>{
-    if(openif==false){
-        langOpen.style.display = 'flex';
-        openif = true //상태값 변경
-    }else if(openif == true){
-        langOpen.style.display = 'none';
-        openif = false;
-    }
-})
+// let openif = false; // 숨기기
+// langSelect.addEventListener('click', ()=>{
+//     if(openif==false){
+//         langOpen.style.display = 'flex';
+//         openif = true //상태값 변경
+//     }else if(openif == true){
+//         langOpen.style.display = 'none';
+//         openif = false;
+//     }
+// })
--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 /* 무림 메인페이지 JS */
-var mySwiper = new Swiper('.swiper', { 
-    slideperview:1,
-    autoplay: {delay:3000,},
-    loop: true,
-})
+// var mySwiper = new Swiper('.swiper', { 
+//     slideperview:1,
+//     autoplay: {delay:3000,},
+//     loop: true,
+// })
