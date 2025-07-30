6b134b1d06f4976886ef4afc9a8f8446ac3ba2f8
jay <wpdl724@gmail.com>
2025-07-30 16:26:28 +0900

    푸터 진행중

--- scripts/index.js
+++ scripts/index.js
@@ -3,4 +3,9 @@ var mySwiper = new Swiper('.swiper', {
     slideperview:1,
     autoplay: {delay:6000,},
     loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true, // bullet 클릭 가능하도록 설정
+    },
 })
+
