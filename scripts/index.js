7b886183b02463420cf8d135419e429f27fba9fb
jay <wpdl724@gmail.com>
2025-08-06 15:11:18 +0900

    1행 bnr 애니메이션 효과, 3행 유튜브 영상수정

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,7 @@ var mySwiper = new Swiper('.swiper', {
     slideperview:1,
     autoplay: {delay:6000,},
     loop: true,
+    effect: 'fade', //부드러운 전환
     pagination: {
         el: '.swiper-pagination',
         clickable: true, // bullet 클릭 가능하도록 설정
